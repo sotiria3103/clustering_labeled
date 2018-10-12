e9532d63b2bffbb2b858a12aa17ec17f3b0d8aa3
sotiria3103 <sotiria3103@gmail.com>
2018-10-12 21:47:56 +0300

    using dictionary instead of class
    Using dictionary instead of class and added distribution graphs

--- clustering_labeled.py
+++ clustering_labeled.py
@@ -2,84 +2,52 @@ import csv
 import pylab as pl
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 
-class Session:
-    """class for sessions"""
-
-    def __init__(self, sessionID, totalTopics, uniqueTopics, uniquePercentage, variance, varianceProbabilistic, human, robot,
-                 numOfRequests, duration, averageTime, standardDeviation, repeated, http0, http2, http3, http4, http5, http7,
-                 percentage_pdf, uniqueContent, multiCountries, webService ):
-        # semantic features
-        self.sessionID = sessionID
-        self.totalTopics = totalTopics
-        self.uniqueTopics = uniqueTopics
-        self.uniquePercentage = uniquePercentage
-        self.variance = variance
-        self.varianceProbabilistic = varianceProbabilistic
-        # simple features
-        self.numOfRequests = numOfRequests
-        self.duration = duration
-        self.averageTime = averageTime
-        self.standardDeviation = standardDeviation
-        self.repeated = repeated
-        self.http0 = http0
-        self.http2 = http2
-        self.http3 = http3
-        self.http4 = http4
-        self.http5 = http5
-        self.http7 = http7
-        self.percentage_pdf = percentage_pdf
-        self.uniqueContent = uniqueContent
-        self.multiCountries = multiCountries
-        self.webService = webService
-        # common features
-        self.human = human
-        self.robot = robot
-
+# gets all sessionIDs in semantic_features file and their index to check later if the session exists in simple_features file
+with open('semantic_features.csv') as semanticfile:
+    semantic_indices = dict((r[0], i) for i, r in enumerate(csv.reader(semanticfile)))
 
 with open('simple_features.csv') as simplefile:
     with open('semantic_features.csv') as semanticfile:
         print("ALL FEATURES")
-        # in case of a header
-        has_header = csv.Sniffer().has_header(semanticfile.readline())
-        semanticfile.seek(0)
-        readSemantic = csv.reader(semanticfile, delimiter=',')
-        if has_header:
-            next(readSemantic)
-        has_header = csv.Sniffer().has_header(simplefile.readline())
-        simplefile.seek(0)
-        readSimple = csv.reader(simplefile, delimiter=',')
-        if has_header:
-            next(readSimple)
 
-        sessions = []
-        for semantic_row, simple_row in zip(readSemantic, readSimple):
-            session = Session(semantic_row[0], int(semantic_row[1]), int(semantic_row[2]), float(semantic_row[3]),
-                              float(semantic_row[4]), float(semantic_row[5]), int(semantic_row[6]), int(semantic_row[7]),
-                              int(simple_row[1]), int(simple_row[2]), float(simple_row[3]), float(simple_row[4]), float(simple_row[5]), float(simple_row[6]),
-                              float(simple_row[7]), float(simple_row[8]), float(simple_row[9]), float(simple_row[10]), float(simple_row[11]), float(simple_row[12]),
-                              int(simple_row[13]), int(simple_row[14]), int(simple_row[15]))
-            sessions.append(session)
+        read_simple = csv.DictReader(simplefile)
+        read_semantic = csv.DictReader(semanticfile)
 
         labeled = []
         labeled_robots = []
+        labeled_index = []
         humans = robots = 0
-        for ses in sessions:
-            if ses.human == 1 or ses.robot == 1:
-                if ses.human == 1:
-                    humans = humans + 1
-                if ses.robot == 1:
-                    robots = robots + 1
-                # list where every column is a feature and every line is a labeled session
-                labeled_robots.append(ses.robot)
-                labeled.append([ses.totalTopics, ses.uniqueTopics, ses.uniquePercentage, ses.variance, ses.varianceProbabilistic,
-                                ses.numOfRequests, ses.duration, ses.averageTime, ses.standardDeviation, ses.repeated, ses.http0,
-                                ses.http2, ses.http3, ses.http4, ses.http5, ses.http7, ses.percentage_pdf, ses.uniqueContent,
-                                ses.multiCountries, ses.webService])
+
+        for semantic_row, simple_row in zip(read_semantic, read_simple):
+            index = semantic_indices.get(simple_row["SessionID"])
+            # print(index)
+            if index is not None:  # the session with id = simple_row["SessionID"] exists in semantic_features file
+                if int(semantic_row["Human"]) == 1 or int(semantic_row["Robot"]) == 1:
+                    if int(semantic_row["Human"]) == 1:
+                        humans = humans + 1
+                    if int(semantic_row["Robot"]) == 1:
+                        robots = robots + 1
+
+                    labeled_index.append(index)
+                    labeled_robots.append(int(semantic_row["Robot"]))
+
+                    labeled.append([int(semantic_row["Total_Topics"]), int(semantic_row["Unique_Topics"]),
+                                    float(semantic_row["Unique_Percentage"]),
+                                    float(semantic_row["Variance"]), float(semantic_row["Variance_Probabilistic"]),
+                                    int(simple_row["#Requests"]), int(simple_row["Duration"]),
+                                    float(simple_row["AverageTime"]), float(simple_row["StandardDeviation"]),
+                                    float(simple_row["Repeated"]), float(simple_row["HTTP0"]),
+                                    float(simple_row["HTTP2"]), float(simple_row["HTTP3"]),
+                                    float(simple_row["HTTP4"]), float(simple_row["HTTP5"]), float(simple_row["HTTP7"]),
+                                    float(simple_row["%PDF"]), int(simple_row["UniqueContent"]),
+                                    int(simple_row["MultiCountries"]), int(simple_row["WebService"])])
 
         print("actual number of humans:", humans)
         print("actual number of robots:", robots)
+
         # Fitting with inputs
         kmeans = KMeans(n_clusters=2).fit(labeled)
         # Predicting the clusters
@@ -99,14 +67,13 @@ with open('simple_features.csv') as simplefile:
                 if labeled_robots[i] == 1:
                     r1 += 1  # robots in second cluster counter
 
-        # (len(labeled_robots))=(len(labeled))=(len(labels))=(len(kmeans.labels_)) labels[j] == kmeans.labels_[j]
-
         print("sessions in first cluster: ", c0)
         print("sessions in second cluster: ", c1)
         print("robots in first cluster: ", r0)
         print("robots in second cluster: ", r1)
         print("percentage of robots in first cluster: ", (r0 / c0) * 100, "%")
         print("percentage of robots in second cluster: ", (r1 / c1) * 100, "%")
+
         # find "natural" k, where the score for k clusters doesn't have a big difference form the score for k+1 clusters
         N = range(1, 20)
         kmeans = [KMeans(n_clusters=i) for i in N]
@@ -128,11 +95,9 @@ with open('simple_features.csv') as simplefile:
         counter = [0]*10
         robot_counter = [0]*10
         for i in range(0, len(labeled)):
-            for j in range(0, 10):
-                if kmeans10.labels_[i] == j:
-                    counter[j] += 1  # j-th cluster counter
-                    if labeled_robots[i] == 1:
-                        robot_counter[j] += 1  # robots in j-th cluster counter
+            counter[kmeans10.labels_[i]] += 1  # cluster counter
+            if labeled_robots[i] == 1:
+                robot_counter[kmeans10.labels_[i]] += 1  # counter for robots in cluster
 
         for i in range(0, 10):
             print("sessions in cluster ", i+1, " : ", counter[i])
@@ -148,10 +113,74 @@ with open('simple_features.csv') as simplefile:
         # Show the plot.
         pl.show()
 
-        pca_2 = PCA(2)
         # Fit the PCA model on the numeric columns from earlier.
         plot_labeled = pca_2.fit_transform(labeled)
         # Make a scatter plot of each labeled session, shaded according to cluster assignment.
         pl.scatter(x=plot_labeled[:, 0], y=plot_labeled[:, 1], c=labels10)
         # Show the plot.
         pl.show()
+
+        duration = []
+        average_time = []
+        standard_deviation = []
+        duration_all = []
+        average_time_all = []
+        standard_deviation_all = []
+
+        for i in range(0, 10):
+            for j, l in enumerate(labeled, start=0):
+                if i == 0:
+                    duration_all.append(l[6])
+                    average_time_all.append(l[7])
+                    standard_deviation_all.append(l[8])
+                if kmeans10.labels_[j] == i:
+                    duration.append(l[6])
+                    average_time.append(l[7])
+                    standard_deviation.append(l[8])
+
+            # Draw the plot
+            plt.subplot(1, 2, 1)
+            plt.title('distribution of duration for cluster {i} '.format(i=i + 1))
+            plt.hist(duration_all, color='red', bins=50, range=(0, 3000), label='all', edgecolor='black')
+            plt.legend(loc='upper right')
+            plt.subplot(1, 2, 2)
+            plt.hist(duration, color='blue', bins=50, label='cluster', edgecolor='black')
+            plt.legend(loc='upper right')
+            plt.savefig("duration_all_features{i}.png".format(i=i + 1), bbox_inches='tight')
+            plt.show()
+
+            plt.subplot(1, 2, 1)
+            plt.title('distribution of average time for cluster {i} '.format(i=i + 1))
+            plt.hist(average_time_all, color='red', bins=50, range=(0, 1000), label='all', edgecolor='black')
+            plt.legend(loc='upper right')
+            plt.subplot(1, 2, 2)
+            plt.hist(average_time, color='blue', bins=50, label='cluster', edgecolor='black')
+            plt.legend(loc='upper right')
+            plt.savefig("average_time_all_features{i}.png".format(i=i + 1), bbox_inches='tight')
+            plt.show()
+
+            plt.subplot(1, 2, 1)
+            plt.title('distribution of standard deviation for cluster {i} '.format(i=i + 1))
+            plt.hist(standard_deviation_all, color='red', bins=50, range=(0, 1000), label='all', edgecolor='black')
+            plt.legend(loc='upper right')
+            plt.subplot(1, 2, 2)
+            plt.hist(standard_deviation, color='blue', bins=50, label='cluster', edgecolor='black')
+            plt.legend(loc='upper right')
+            plt.savefig("standard_deviation_all_features{i}.png".format(i=i + 1), bbox_inches='tight')
+            plt.show()
+
+            print("min duration in cluster ", i + 1, ": ", min(duration))
+            print("max duration in cluster ", i + 1, ": ", max(duration))
+            print("range: ", max(duration) - min(duration))
+
+            print("min average time in cluster ", i + 1, ": ", min(average_time))
+            print("max average time in cluster ", i + 1, ": ", max(average_time))
+            print("range: ", max(average_time) - min(average_time))
+
+            print("min standard deviation in cluster ", i + 1, ": ", min(standard_deviation))
+            print("max standard deviation in cluster ", i + 1, ": ", max(standard_deviation))
+            print("range: ", max(standard_deviation) - min(standard_deviation))
+
+            duration[:] = []
+            average_time[:] = []
+            standard_deviation[:] = []
--- clustering_semantic.py
+++ clustering_semantic.py
@@ -2,76 +2,33 @@ import csv
 import pylab as pl
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
-# source: http://www.michaeljgrogan.com/k-means-clustering-python-sklearn/
-
-
-class Session:
-    """class for sessions"""
-
-    def __init__(self, sessionID, totalTopics, uniqueTopics, uniquePercentage, variance, varianceProbabilistic, human, robot):
-        # semantic features
-        self.sessionID = sessionID
-        self.totalTopics = totalTopics
-        self.uniqueTopics = uniqueTopics
-        self.uniquePercentage = uniquePercentage
-        self.variance = variance
-        self.varianceProbabilistic = varianceProbabilistic
-        self.human = human
-        self.robot = robot
-
+import matplotlib.pyplot as plt
 
 with open('semantic_features.csv') as semanticfile:
     print("SEMANTIC FEATURES")
-    # in case of a header
-    has_header = csv.Sniffer().has_header(semanticfile.readline())
-    semanticfile.seek(0)
-    readSemantic = csv.reader(semanticfile, delimiter=',')
-    if has_header:
-        next(readSemantic)
 
-    sessions = []
-    for semantic_row in readSemantic:
-        session = Session(semantic_row[0], int(semantic_row[1]), int(semantic_row[2]), float(semantic_row[3]),
-                          float(semantic_row[4]), float(semantic_row[5]), int(semantic_row[6]), int(semantic_row[7]))
-        sessions.append(session)
-    unlabeled = []
+    read_semantic = csv.DictReader(semanticfile)
+
     labeled = []
     labeled_robots = []
+    labeled_index = []
     humans = robots = 0
-    for ses in sessions:
-        # if ses.human == 0 and ses.robot == 0:
-            # 2d matrix where every column is a feature and every line is an unlabeled session
-            # unlabeled.append([ses.totalTopics, ses.uniqueTopics, ses.uniquePercentage, ses.variance, ses.varianceProbabilistic])
-
-        if ses.human == 1 or ses.robot == 1:
-            if ses.human == 1:
+    for index, semantic_row in enumerate(read_semantic, start=0):
+        if int(semantic_row["Human"]) == 1 or int(semantic_row["Robot"]) == 1:
+            if int(semantic_row["Human"]) == 1:
                 humans = humans + 1
-            if ses.robot == 1:
+            if int(semantic_row["Robot"]) == 1:
                 robots = robots + 1
-                # 2d matrix where every column is a feature and every line is a labeled session
-            labeled_robots.append(ses.robot)
-            labeled.append([ses.totalTopics, ses.uniqueTopics, ses.uniquePercentage, ses.variance, ses.varianceProbabilistic])
 
-    # Fitting with inputs
-    # kmeans = KMeans(n_clusters=2).fit(unlabeled)
-    # Predicting the clusters
-    # labels = kmeans.predict(unlabeled)
-    # Getting the cluster centers
-    # C_unlabeled = kmeans.cluster_centers_
-    # print("centers for two clusters (unlabeled): ", C_unlabeled)
-    # c_u0 = c_u1 = i = 0
-    # for u in unlabeled:
-    #    if kmeans.labels_[i] == 1:
-    #        c_u1 = c_u1 + 1
-    #    if kmeans.labels_[i] == 0:
-    #        c_u0 = c_u0 + 1
-    #    i = i + 1
+            labeled_index.append(index)
+            labeled_robots.append(int(semantic_row["Robot"]))
 
-    # print("sessions in first cluster: ", c_u0)
-    # print("sessions in second cluster: ", c_u1)
+            labeled.append([int(semantic_row["Total_Topics"]), int(semantic_row["Unique_Topics"]), float(semantic_row["Unique_Percentage"]),
+                            float(semantic_row["Variance"]), float(semantic_row["Variance_Probabilistic"])])
 
     print("actual number of humans:", humans)
     print("actual number of robots:", robots)
+
     # Fitting with inputs
     kmeans = KMeans(n_clusters=2).fit(labeled)
     # Predicting the clusters
@@ -99,16 +56,6 @@ with open('semantic_features.csv') as semanticfile:
     print("percentage of robots in second cluster: ", (r1 / c1) * 100, "%")
 
     # find "natural" k, where the score for k clusters doesn't have a big difference from the score for k+1 clusters
-    # N = range(1, 20)
-    # kmeans_unlabeled = [KMeans(n_clusters=i) for i in N]
-    # score = [kmeans_unlabeled[i].fit(unlabeled).score(unlabeled) for i in range(len(kmeans_unlabeled))]
-    # pl.plot(N, score)
-    # pl.xlabel('Number of Clusters')
-    # pl.ylabel('Score')
-    # pl.title('Elbow Curve')
-    # pl.show()
-
-    # find "natural" k, where the score for k clusters doesn't have a big difference from the score for k+1 clusters
     N = range(1, 20)
     kmeans_labeled = [KMeans(n_clusters=i) for i in N]
     score = [kmeans_labeled[i].fit(labeled).score(labeled) for i in range(len(kmeans_labeled))]
@@ -120,7 +67,7 @@ with open('semantic_features.csv') as semanticfile:
 
 
     # Fitting with inputs
-    kmeans6 = KMeans(n_clusters=6).fit(labeled)
+    kmeans6 = KMeans(n_clusters=6).fit(labeled) # k=6
     # Predicting the clusters
     labels6 = kmeans6.predict(labeled)
     # Getting the cluster centers
@@ -130,18 +77,16 @@ with open('semantic_features.csv') as semanticfile:
     counter = [0] * 6
     robot_counter = [0] * 6
     for i in range(0, len(labeled)):
-        for j in range(0, 6):
-            if kmeans6.labels_[i] == j:
-                counter[j] += 1  # j-th cluster counter
-                if labeled_robots[i] == 1:
-                    robot_counter[j] += 1  # robots in j-th cluster counter
+        counter[kmeans6.labels_[i]] += 1  # cluster counter
+        if labeled_robots[i] == 1:
+                robot_counter[kmeans6.labels_[i]] += 1  # counter for robots in cluster
 
     for i in range(0, 6):
         print("sessions in cluster ", i + 1, " : ", counter[i])
         print("robots in cluster ", i + 1, " : ", robot_counter[i])
         print("percentage of robots in cluster ", i + 1, " : ", (robot_counter[i] / counter[i]) * 100, "%")
 
-        # Create a PCA model.
+    # Create a PCA model.
     pca_2 = PCA(2)
     # Fit the PCA model on the numeric columns from earlier.
     plot_labeled = pca_2.fit_transform(labeled)
@@ -150,10 +95,56 @@ with open('semantic_features.csv') as semanticfile:
     # Show the plot.
     pl.show()
 
-    pca_2 = PCA(2)
     # Fit the PCA model on the numeric columns from earlier.
     plot_labeled = pca_2.fit_transform(labeled)
+    plot_c = pca_2.fit_transform(C)
     # Make a scatter plot of each labeled session, shaded according to cluster assignment.
     pl.scatter(x=plot_labeled[:, 0], y=plot_labeled[:, 1], c=labels6)
     # Show the plot.
     pl.show()
+
+    total_topics = []
+    unique_topics = []
+    total_topics_all = []
+    unique_topics_all = []
+
+    for i in range(0, 6):
+        for j, l in enumerate(labeled, start=0):
+            if i == 0:
+                total_topics_all.append(l[0])
+                unique_topics_all.append(l[1])
+            if kmeans6.labels_[j] == i:
+                total_topics.append(l[0])
+                unique_topics.append(l[1])
+
+        # Draw the plot
+
+        plt.subplot(1, 2, 1)
+        plt.title('distribution of total topics for cluster {i} '.format(i=i + 1))
+        plt.hist(total_topics_all, color='red', bins=50, range=(0, 2000), label='all', edgecolor='black')
+        plt.legend(loc='upper right')
+        plt.subplot(1, 2, 2)
+        plt.hist(total_topics, color='blue', bins=30, label='cluster', edgecolor='black')
+        plt.legend(loc='upper right')
+        plt.savefig("total_topics{i}.png".format(i=i + 1), bbox_inches='tight')
+        plt.show()
+
+        plt.subplot(1, 2, 1)
+        plt.title('distribution of unique topics for cluster {i} '.format(i=i + 1))
+        plt.hist(unique_topics_all, color='red', bins=50, range=(0, 1000), label='all', edgecolor='black')
+        plt.legend(loc='upper right')
+        plt.subplot(1, 2, 2)
+        plt.hist(unique_topics, color='blue', bins=30, label='cluster', edgecolor='black')
+        plt.legend(loc='upper right')
+        plt.savefig("unique_topics{i}.png".format(i=i + 1), bbox_inches='tight')
+        plt.show()
+
+        print("min total topics in cluster ", i + 1, ": ", min(total_topics))
+        print("max total topics in cluster ", i + 1, ": ", max(total_topics))
+        print("range: ", max(total_topics)-min(total_topics))
+        print("min unique topics in cluster ", i + 1, ": ", min(unique_topics))
+        print("max unique topics in cluster ", i + 1, ": ", max(unique_topics))
+        print("range: ", max(unique_topics) - min(unique_topics))
+
+        total_topics[:] = []
+        unique_topics[:] = []
